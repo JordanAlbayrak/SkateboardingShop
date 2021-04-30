f5464588bbe85d3350c118eed7e612b159d5ab9e
Iana-Sha <70303879+Iana-Sha@users.noreply.github.com>
2021-04-30 16:51:11 -0400

    Website design modified
    Created link, pictures and pages for  "longboards " and "skateboards"  shopping pages.
    Changed the active class on every navbar to match the current page.
    Added pictures, improved overall design.
    Added videos in the "about us" section
    Fixed the home page link
    Added dropdown in nav for shop containing "longboards " and "skateboards" pages.

---
+++ skateshop/html-css-img/longboards.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Skaters Crew</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+    <link rel = "stylesheet" type = "text/css" href = "style.css">
+</head>
+<body class = "mb-5">
+<div class ="row bg-white ">
+    <header style = "padding-left: 9.09%" class = "text-center display-4 col-sm-12 col-md-10 col-lg-11">
+        Skater Crew Longboards
+    </header>
+    <div class = "text-center col-sm-12 col-md-2 col-lg-1">
+        <button style = "width: 100%" class="mt-1 mr-1"><a href = "http://localhost/php_final/Skateboarding_Shop_php/login.php"class = "mr-2">Log in</a></button>
+        </br>
+        <button style = "width: 100%" class="mr-1"><a href = "http://localhost/php_final/Skateboarding_Shop_php/entry.php"class = "mr-2">Shop Cart</a></button>
+    </div>
+</div>
+<div class="container-fluid">
+
+    <!--NAVIGATION MENU-->
+
+    <div class = "row bg-white p-0">
+        <nav class="justify-content-between navbar navbar-expand-sm bg-dark navbar-dark mx-0 col-sm-12 col-md-12 col-lg-12">
+
+            <ul class="navbar-nav">
+                <li class="nav-item" >
+                    <a class="nav-link" href = "index.php">Home</a>
+
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="skateboards.php">Skateboards</a>
+                        <a class="dropdown-item" href="longboards.php">Longboards</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href = "contact.php">Contact</a>
+                </li>
+            </ul>
+            <div class="float-right">
+                <form class="nav-item form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+</body>
+</html>
